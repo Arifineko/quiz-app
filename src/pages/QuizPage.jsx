79593e689cee7cc491166274b658da03c3c2a89d
arifin <aeko86869@gmail.com>
2026-06-13 22:37:14 +0700

    feat: enhance quiz resume functionality and clear storage on submit

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -5,7 +5,7 @@ import { useQuizReview } from "../hooks/useQuizReview";
 import QuizResult from "../components/quiz/QuizResult";
 import QuestionCard from "../components/quiz/QuestionCard";
 import QuizTimer from "../components/quiz/QuizTimer";
-import { getFromStorage, saveToStorage } from "../services/storage";
+import { getFromStorage, removeFromStorage, saveToStorage } from "../services/storage";
 
 const QuizPage = () => {
     const [answer, setAnswer] = useState(getFromStorage('quiz-answer') || {});
@@ -15,14 +15,32 @@ const QuizPage = () => {
 
     useEffect(() => {
         const loadQuizData = async () => {
-            if (localStorage.getItem('quiz-data')) {
-                const quizData = getFromStorage('quiz-data')
-                setData(quizData);
-                return;
+            const savedQuizData = getFromStorage("quiz-data")
+            const savedAnswer = getFromStorage("quiz-answer")
+
+            if (savedQuizData) {
+                setData(savedQuizData)
+
+                if (savedAnswer) {
+                    const isResume = confirm("Do you want to resume your quiz?")
+
+                    if (isResume) {
+                        setAnswer(savedAnswer)
+                    } else {
+                        localStorage.removeItem("quiz-answer")
+                        localStorage.removeItem("quiz-data")
+                        localStorage.removeItem("quiz-timer")
+                        setAnswer({})
+                    }
+                }
+
+                return
             }
-            const quizData = await getQuizData();
-            setData(quizData);
-            saveToStorage('quiz-data', quizData)
+
+            const quizData = await getQuizData()
+
+            setData(quizData)
+            saveToStorage("quiz-data", quizData)
         };
 
         loadQuizData();
@@ -32,6 +50,10 @@ const QuizPage = () => {
         reviewAnswer()
         const decision = confirm('are you sure to submit your answer?')
         if (decision) {
+            removeFromStorage('quiz-answer')
+            removeFromStorage('quiz-timer')
+            removeFromStorage('quiz-data')
+            setAnswer({})
             setIsDone(true)
         }
     }
